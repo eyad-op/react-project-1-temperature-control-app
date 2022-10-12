053d0cb260a95d4e5bc75e8dd99ce54278c84597
eyad-op <43111541+eyad-op@users.noreply.github.com>
2022-10-12 11:29:21 +0300

    setting up the app interface

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <div className="temperature-display-container">
+        <div className="temperature-display">10°C</div>
+      </div>
+      <div className="button-container">
+        <button>-</button>
+        <button>+</button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
