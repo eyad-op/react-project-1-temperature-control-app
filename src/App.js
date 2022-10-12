aeee06038156a310d82f086a6ae2ade56d0b9047
eyad-op <43111541+eyad-op@users.noreply.github.com>
2022-10-12 12:04:39 +0300

    finished project

--- src/App.js
+++ src/App.js
@@ -2,26 +2,37 @@ import React, { useState } from "react";
 
 const App = () => {
   const [temperatureValue, setTemperatureValue] = useState(10);
+  const [temperatureColor, setTemperatureColor] = useState("cold");
+
+  const setColor = (temperature) => {
+    return temperature < 16
+      ? setTemperatureColor("cold")
+      : setTemperatureColor("hot");
+  };
+
+  const decreaseTemperature = () => {
+    if (temperatureValue <= 0) return;
+    const newTemperature = temperatureValue - 1;
+    setColor(newTemperature);
+    setTemperatureValue(newTemperature);
+  };
+  const increaseTemperature = () => {
+    if (temperatureValue >= 25) return;
+    const newTemperature = temperatureValue + 1;
+    setColor(newTemperature);
+    setTemperatureValue(newTemperature);
+  };
+
   return (
     <div className="app-container">
       <div className="temperature-display-container">
-        <div className="temperature-display">{temperatureValue}°C</div>
+        <div className={`temperature-display ${temperatureColor}`}>
+          {temperatureValue}°C
+        </div>
       </div>
       <div className="button-container">
-        <button
-          onClick={() =>
-            setTemperatureValue((currentValue) => currentValue - 1)
-          }
-        >
-          -
-        </button>
-        <button
-          onClick={() =>
-            setTemperatureValue((currentValue) => currentValue + 1)
-          }
-        >
-          +
-        </button>
+        <button onClick={() => decreaseTemperature()}>-</button>
+        <button onClick={() => increaseTemperature()}>+</button>
       </div>
     </div>
   );
